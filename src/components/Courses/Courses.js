6e5caa7bb5c28fa745a219b0ee7eacf95d7f6e24
rinanasir <rinanasir25@gmail.com>
2021-10-05 16:49:28 +0600

    added courses

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>courses</h1>
+        </div>
+    );
+};
+
+export default Courses;
--- src/components/Learn/Learn.js
+++ src/components/Learn/Learn.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useEffect, useState } from 'react/cjs/react.development';
+import Courses from '../Courses/Courses';
 
 const Learn = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('./courses.JSON')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, []);
+
     return (
         <div>
-            <h1>this is learn</h1>
+            <h1 className="text-center text-4xl font-bold text-pink-500 mt-4 pb-6">What you can Learn</h1>
+            <div className="grid grid-cols-4 gap-4 px-6">
+                {
+                    courses.map(course => <Courses
+                        key={course.key}
+                        serviceItem={course}
+                    ></Courses>)
+                }
+                <h1 className="text-center text-4xl font-bold text-pink-500 mt-20">More courses are comming soon...</h1>
+            </div>
         </div>
     );
 };
